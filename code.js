ac5abc0c6774dec387edb61e949034b296e243e4
Russo <yaleksander42@gmail.com>
2025-07-20 21:13:58 -0300

    add sync solution to handle command flow

--- code.js
+++ code.js
@@ -7,12 +7,42 @@ const infoVar = RPM.Manager.Plugins.getParameter(pluginName, "Text info variable
 var currentGame = null;
 var settingsName = "Enable Settings on Systems tab!";
 
+class WaitSaveLoadAsync extends RPM.EventCommand.Base
+{
+	constructor()
+	{
+		super();
+		this.asyncSaveLoadFinished = false;
+	}
+
+	update(currentState)
+	{
+		return this.asyncSaveLoadFinished;
+	}
+}
+
+class WaitSaveLoadScene extends RPM.EventCommand.Base
+{
+	constructor()
+	{
+		super();
+		this.prevScene = RPM.Manager.Stack.top;
+		this.beginCheck = false;
+	}
+
+	update(currentState)
+	{
+		return this.beginCheck && this.prevScene === RPM.Manager.Stack.top;
+	}
+}
+
 RPM.Scene.TitleScreen.prototype.load = async function ()
 {
 	const l = RPM.Datas.TitlescreenGameover.titleCommands;
 	for (var i = 0; i < l.length; i++)
 		if (l[i].kind === RPM.Common.Enum.TitleCommandKind.Settings)
 			settingsName = l[i].name();
+	this.title = settingsName;
 	RPM.Core.Game.current = null;
 	RPM.Manager.Videos.stop();
 	RPM.Manager.Songs.stopAll();
@@ -31,15 +61,44 @@ RPM.Scene.LoadGame.prototype.cancel = function (isKey, options = {})
 		RPM.Manager.Stack.pop();
 		RPM.Manager.Stack.pop();
 	}
+};
+
+function addSaveLoadWaitCommand(alt = false)
+{
+	const c = RPM.Core.ReactionInterpreter.currentReaction.currentCommand;
+	if (!c.hasSaveLoadWaitCommand)
+	{
+		c.hasSaveLoadWaitCommand = true;
+		const n = c.next;
+		if (!alt)
+			c.next = new RPM.Core.Node(c.parent, new WaitSaveLoadAsync());
+		else
+			c.next = new RPM.Core.Node(c.parent, new WaitSaveLoadScene());
+		c.next.next = n;
+	}
+	else
+	{
+		if (!alt)
+			c.next.asyncSaveLoadFinished = false;
+		else
+		{
+			c.beginCheck = false;
+			c.next.prevScene = RPM.Manager.Stack.top;
+		}
+	}
+	return c.next;
 }
 
 RPM.Manager.Plugins.registerCommand(pluginName, "Save slot", (slot) =>
 {
+	const waitCommand = addSaveLoadWaitCommand();
 	RPM.Core.Game.current.save(slot);
+	waitCommand.data.asyncSaveLoadFinished = true;
 });
 
 RPM.Manager.Plugins.registerCommand(pluginName, "Load slot", async (slot) =>
 {
+	const waitCommand = addSaveLoadWaitCommand();
 	if (await RPM.Common.IO.fileExists(RPM.Common.Paths.SAVES + "/" + slot + ".json"))
 	{
 		RPM.Datas.Systems.soundConfirmation.playSound();
@@ -53,10 +112,12 @@ RPM.Manager.Plugins.registerCommand(pluginName, "Load slot", async (slot) =>
 	}
 	else
 		RPM.Datas.Systems.soundImpossible.playSound();
+	waitCommand.data.asyncSaveLoadFinished = true;
 });
 
 RPM.Manager.Plugins.registerCommand(pluginName, "Delete slot", async (slot) =>
 {
+	const waitCommand = addSaveLoadWaitCommand();
 	const fs = require("fs").promises;
 	try
 	{
@@ -66,47 +127,52 @@ RPM.Manager.Plugins.registerCommand(pluginName, "Delete slot", async (slot) =>
 	{
 		console.error(e);
 	}
+	waitCommand.data.asyncSaveLoadFinished = true;
 });
 
 RPM.Manager.Plugins.registerCommand(pluginName, "Slot exists?", async (slot, variable) =>
 {
+	const waitCommand = addSaveLoadWaitCommand();
 	RPM.Core.Game.current.variables[variable] = -1;
 	RPM.Core.Game.current.variables[variable] = await RPM.Common.IO.fileExists(RPM.Common.Paths.SAVES + "/" + slot + ".json");
+	waitCommand.data.asyncSaveLoadFinished = true;
 });
 
-RPM.Manager.Plugins.registerCommand(pluginName, "Get slot info", async (slot, info, time) =>
+RPM.Manager.Plugins.registerCommand(pluginName, "Get slot variable", async (slot, variable, result, loading, error) =>
 {
-	const p = RPM.Core.ReactionInterpreter.currentObject;
+	const waitCommand = addSaveLoadWaitCommand();
+	const v = RPM.Core.Game.current.variables;
 	const game = new RPM.Core.Game(slot);
-	p.properties[info] = null;
+	v[loading] = true;
+	v[error] = false;
 	try
 	{
 		await game.load();
-		var s = game.playTime.getSeconds();
-		const m = Math.floor(s / 60).toString();
-		const h = Math.floor(s / 3600).toString();
-		s = (s % 60).toString();
-		p.properties[info] = game.variables[infoVar];
-		p.properties[time] = (h.length === 1 ? "0" : "") + h + ":" + (m.length === 1 ? "0" : "") + m + ":" + (s.length === 1 ? "0" : "") + s;
+		v[result] = game.variables[variable];
 	}
-	catch (error)
+	catch (e)
 	{
-		p.properties[info] = "";
-		p.properties[time] = "";
-		console.log(error);
+		v[error] = true;
+		console.error(e);
 	}
+	v[loading] = false;
+	waitCommand.data.asyncSaveLoadFinished = true;
 });
 
 RPM.Manager.Plugins.registerCommand(pluginName, "Open load menu", () =>
 {
+	const waitCommand = addSaveLoadWaitCommand(true);
 	currentGame = RPM.Core.Game.current;
 	RPM.Manager.Stack.push(new RPM.Scene.Base());
 	RPM.Manager.Stack.push(new RPM.Scene.LoadGame());
+	waitCommand.data.beginCheck = true;
 });
 
 RPM.Manager.Plugins.registerCommand(pluginName, "Open settings menu", () =>
 {
+	const waitCommand = addSaveLoadWaitCommand(true);
 	RPM.Manager.Stack.push(new RPM.Scene.TitleSettings(settingsName));
+	waitCommand.data.beginCheck = true;
 });
 
 RPM.Manager.Plugins.registerCommand(pluginName, "Quit game", () =>
